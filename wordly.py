a4686ef154af69c87ec0c77b8e8069bb5f81f7c1
nikitadet <168132134+nikitadet@users.noreply.github.com>
2025-04-16 19:20:56 +0300

    покраска букв

--- wordly.py
+++ wordly.py
@@ -1,16 +1,46 @@
 import random
+import colorama
+from colorama import Fore, Back, Style
 
 
+colorama.init()
+words = open("slova5bykvwordly", "r", encoding='utf-8').read().split("\n")
+randomslowo = random.choice(words)
+
+
+def solo ():
+     for i in range(5):
+        a = input()
+        if len(a) > 5:
+            print("слово длинее 5 букв")
+            break
+        if a == randomslowo:
+            print("вы угадали слово, потребовалось попыток:" + i)
+        for k in range(5):
+            if a[k] in randomslowo:
+                b = randomslowo.find(a[k])
+                if k == b:
+                    c = a[k]
+                    print(Back.GREEN + c, end="")
+                else:
+                    print(Back.YELLOW + a[k], end="")
+            else:
+                print(Style.RESET_ALL + a[k], end="")
+def duo ():
+     print("duo")
 
 print("вы играете solo или duo")
 while True:
     play = input()
     if play == "solo" or play == "duo":
+            if play == "solo":
+                 solo()
+            else:
+                 duo()
             break
     else:
         print("если играете один то пишите solo, если вдвоем, то duo")
         continue
 
-words = open("slova5bykvwordly", "r", encoding='utf-8').read().split("\n")
-randomslowo = random.choice(words)
-print(randomslowo)
+
+
