a925ef21b4fddca29107e5b567b8ee0ccd430b54
nikitadet <168132134+nikitadet@users.noreply.github.com>
2025-04-16 18:15:35 +0300

    обработка файла + рандомное слово

--- wordly.py
+++ wordly.py
@@ -1,3 +1,7 @@
+import random
+
+
+
 print("вы играете solo или duo")
 while True:
     play = input()
@@ -6,3 +10,7 @@ while True:
     else:
         print("если играете один то пишите solo, если вдвоем, то duo")
         continue
+
+words = open("slova5bykvwordly", "r", encoding='utf-8').read().split("\n")
+randomslowo = random.choice(words)
+print(randomslowo)
